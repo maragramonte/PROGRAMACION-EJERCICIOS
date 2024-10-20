423a702c1e042710ae240b81203995c12c3e0b46
maragramonte <mar.s414752@cesurformacion.com>
2024-10-20 17:42:03 +0200

    Delete Mar.java

--- Mar.java
+++
@@ -1,5 +0,0 @@
-public class mar {
-  public static void main(String[] args) {
-    System.out.println("Hola me llamo mar agramonte"); // Con esto debe aparecer nuestro nombre impreso en la pantalla
-  }
-}
